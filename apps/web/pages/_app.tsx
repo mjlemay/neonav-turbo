26681d1f4e23920bf3168a904fb22ba7351134e4
Michael LeMay <mjlemay@gmail.com>
2023-01-16 11:29:00 -0800

    add base styles for web

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,5 +1,14 @@
-import type { AppProps } from "next/app";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/jura/400.css";
+import "@fontsource/jura/500.css";
+import "@fontsource/jura/700.css";
+import "../styles/vars.css";
 import "../styles/global.css";
+import "../styles/augmented-ui.min.css";
+import type { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
